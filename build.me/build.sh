93ad4377f389925d91148ea5760eadf301ff1b54
Michal Radecki <odrzutowiec@gmail.com>
2020-06-10 00:32:38 +0200

    wip removing db

--- build.me/build.sh
+++ build.me/build.sh
@@ -1,9 +1,9 @@
 #!/bin/bash -x
 
-cd ../dist;
-./distrib
-cd -;
-../dist/configure --enable-shared=no --enable-static=no --enable-widechar --enable-dynamic-loading --enable-threads
-make
-sudo rm -rf /usr/local/bin/vip
+cd ../dist &&\
+./distrib &&\
+cd - &&\
+../dist/configure --enable-shared=no --enable-static=no --enable-widechar --enable-threads &&\
+make &&\
+sudo rm -rf /usr/local/bin/vip &&\
 sudo cp vi /usr/local/bin/vip
